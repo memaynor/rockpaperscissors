740f643c0c3accf555a4cf03a6e57e939cdfe54f
MMaynor <mr.maynor20@gmail.com>
2026-02-11 12:26:47 +0700

    final

--- rps.py
+++ rps.py
@@ -1,7 +1,23 @@
 import random
 
 choices = ["rock","paper","scissors"]
-computer = random.choice(choices)
-player = input("Input rock, paper, or scissors: ").lowercase
+computerscore = 0
+userscore = 0
 
-if 
+
+while(computerscore < 2 and userscore < 2):
+    computer = random.choice(choices)
+    user = input("Input rock, paper, or scissors: ").lower()
+    if(computer == user):
+        print("Tie")
+    elif(user == "rock" and computer == "scissors") or (user == "paper" and computer == "rock") or (user == "scissors" and computer == "paper"):
+        print("You won that hand!")
+        userscore += 1
+    else:
+        print("You lost that hand!")
+        computerscore += 1
+
+if(userscore == 2):
+    print("You win!")
+else:
+    print("The computer won :(")
